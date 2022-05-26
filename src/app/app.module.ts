7157d41b1069c731aa248990f3e85d84baca9f12
Luiz F3lipe <luiz.felipe.moraes.lima.1@gmail.com>
2022-05-25 22:07:34 -0300

    add angular/material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { TempoAtualComponent } from './tempo-atual/tempo-atual.component';
 import { TempoService } from './tempo/tempo.service';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module'
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
@@ -14,6 +16,9 @@ import { HttpClientModule } from '@angular/common/http'
   imports: [
     BrowserModule,
     HttpClientModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FlexLayoutModule
   ],
   providers: [TempoService],
   bootstrap: [AppComponent]
